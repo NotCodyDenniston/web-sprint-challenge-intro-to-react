442fa81fd9cf5d2548e5bf0c166444c08a4159c1
Cody Denniston <dennistonc4817@gmail.com>
2022-03-24 20:02:26 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,15 +11,31 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  axios.get(`https://swapi.dev/api/people`)
-  .then(res => {
-    console.log(res)
-  })
-
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => {
+        setJedi(res.data.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+  
+  const StyledDiv = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+    text-align:justify
+    `
+  
   return (
-    <div className="App">
+    <StyledDiv className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      {jedi.map(jed => {
+        return <Character name={jed.name} height={jed.height} mass={jed.mass} hair_color={jed.hair_color} skin_color={jed.skin_color} />
+       })}
+    </StyledDiv>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+
+    const StyledDiv = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content:center;
+    `
+    const StyledH3 = styled.h3`
+    padding:0px 50px;
+    border:black 1px solid
+    `
+
+return (
+    <StyledDiv>
+        <h2>{props.name}</h2>
+        <StyledH3>{props.height}</StyledH3>
+        <StyledH3>{props.mass}</StyledH3>
+        <StyledH3>{props.hair_color}</StyledH3>
+        <StyledH3>{props.skin_color}</StyledH3>
+    </StyledDiv>
+)
+}
+
+export default Character
