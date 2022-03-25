c0c4e7e1f48a5ceaf169b548c80d4a5f7f03cd81
Cody Denniston <dennistonc4817@gmail.com>
2022-03-24 19:14:33 -0500

    started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [jedi, setJedi] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  axios.get(`https://swapi.dev/api/people`)
+  .then(res => {
+    console.log(res)
+  })
 
   return (
     <div className="App">
